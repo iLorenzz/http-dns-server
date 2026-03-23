43bb673c57fb14caca3f33bff9edf2b980e6b495
iLorenzz <ldutra0912@gmail.com>
2026-03-23 01:16:59 -0300

    feat: read named conf file from txt and store in array

--- dns-chaching-only/config-dns.sh
+++ dns-chaching-only/config-dns.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+NAMED_CONF_FILE="named-conf.txt"
+
+named_conf_list=()
+while CONF= read -r line
+do
+    named_conf_list+=("$line")
+done < "$NAMED_CONF_FILE"
+
 [ ! -x "$(which bind)" ] && yum install bind -y
 
 systemctl enable named
@@ -9,4 +17,6 @@ if [ $? -ne 0  ]; then
     systemctl enable firewalld
     firewall-cmd --add-service=dns --reload
 fi
+
+
 

54daf999ad1c630a71c32d169a672a06fd36f38f
iLorenzz <ldutra0912@gmail.com>
2026-03-24 00:25:20 -0300

    feat: implement starting dns service, configuring firewall and adding dnms configurations in /etc/named.conf

--- dns-chaching-only/config-dns.sh
+++ dns-chaching-only/config-dns.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
 
-NAMED_CONF_FILE="named-conf.txt"
+NAMED_CONF_CONTENT_FILE="named-conf.txt" #$1
+NAMED_CONF_FILE="/etc/named.conf"
 
-named_conf_list=()
-while CONF= read -r line
-do
-    named_conf_list+=("$line")
-done < "$NAMED_CONF_FILE"
+#named_conf_list=()
+#while CONF= read -r line
+#do
+#    named_conf_list+=("$line")
+#done < "$NAMED_CONF_FILE"
+
+yum update -y
 
 [ ! -x "$(which bind)" ] && yum install bind -y
 
 systemctl enable named
+systemctl start named
 
-firewall-cmd --add-service=dns --reload
+firewall-cmd --add-service=dns --permanent
 if [ $? -ne 0  ]; then 
     systemctl enable firewalld
-    firewall-cmd --add-service=dns --reload
+    systemctl start firewalld
+    firewall-cmd --add-service=dns --permanent
 fi
+firewall-cmd --reload
 
+cat $NAMED_CONF_CONTENT_FILE > $NAMED_CONF_FILE
+systemctl restart named
 
-
+named-checkconf
